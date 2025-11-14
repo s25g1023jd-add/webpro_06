1afd32bb1085d547263b4fdb60e13773c5bc0c85
Shi Ou <wang@ounoMacBook-Air.local>
2025-11-14 11:55:26 +0900

    一覧ページと詳細ページを追加し、必要な修正を反映

--- app4.js
+++ app4.js
@@ -23,6 +23,10 @@ app.get("/omikuji1", (req, res) => {
   let luck = '';
   if( num==1 ) luck = '大吉';
   else if( num==2 ) luck = '中吉';
+  else if( num==3) luck = '小吉';
+  else if (num==4) luck = '末吉';
+  else if (num ==5) luck =  '凶';
+  else if (num == 6) luck = '大凶';
 
   res.send( '今日の運勢は' + luck + 'です' );
 });
